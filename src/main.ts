837b76a748b6401ae0b5f9c036c3c63c4d394167
christianlewis21 <christianelewis64@gmail.com>
2025-12-29 17:43:31 -0500

    render leaderboard and helper

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { ButtonAction, ControllerIndex, NullItemID } from "isaac-typescript-definitions";
+import { ButtonAction, ControllerIndex } from "isaac-typescript-definitions";
 import {
   MainMenuType,
   ModCallbackRepentogon,
@@ -11,16 +11,17 @@ leaderboard.Load("gfx/ui/leaderboard/leaderboard.anm2", true);
 leaderboard.Play("Appear", true);
 
 const helper = Sprite();
-helper.Load("gfx/ui/helper/seedwidget.anm2", true);
+helper.Load("gfx/ui/helper/helper.anm2", true);
 helper.Play("Idle", true);
 
-const infobox = Sprite();
-infobox.Load("gfx/ui/infobox/infobox.anm2", true);
-infobox.Play("Appear", true);
+const loading = Sprite();
+loading.Load("gfx/ui/infobox/infobox.anm2", true);
+loading.Play("Appear", true);
 // End Sprites
 
 // Booleans
-let showLeaderboard = false
+let SHOW_LEADERBOARD = false;
+let GLOBAL_SELECTED = true;
 // End Booleans
 export function main(): void {
   const modVanilla = RegisterMod("Stats Leaderboard", 1);
@@ -33,45 +34,77 @@ export function main(): void {
   );
   mod.AddCallbackRepentogon(
     ModCallbackRepentogon.POST_MAIN_MENU_RENDER,
-    updateLeaderboard,
-  );
-  mod.AddCallbackRepentogon(
-    ModCallbackRepentogon.POST_MAIN_MENU_RENDER,
     renderLeaderboard,
   );
 }
 
 function renderLeaderboard() {
   if (MenuManager.GetActiveMenu() === MainMenuType.STATS) {
-    if (Input.IsActionTriggered(
-        ButtonAction.MENU_TAB,
-        ControllerIndex.KEYBOARD,
-      )
-        || Input.IsActionTriggered(
-          ButtonAction.MENU_TAB,
-          ControllerIndex.CONTROLLER_1,
-        )) {
-          showLeaderboard = !showLeaderboard
-        }
-    if (showLeaderboard) {
-      leaderboard.Update()
-      leaderboard.Render(Vector(120, 120))
+    renderHelper();
+    if (Input.IsActionTriggered(ButtonAction.MAP, ControllerIndex.KEYBOARD)) {
+      Isaac.DebugString("Tab Clicked");
+      SHOW_LEADERBOARD = !SHOW_LEADERBOARD;
+      GLOBAL_SELECTED = true;
+    }
+    if (SHOW_LEADERBOARD) {
+      Isaac.DebugString("Showing leaderboard");
+      leaderboard.Update();
+      leaderboard.Render(Vector(0, 0));
       if (leaderboard.IsFinished("Appear")) {
-        leaderboard.Play("Idle", true)
+        Isaac.DebugString("Playing Idle anim");
+        leaderboard.Play("Idle", true);
+      }
+      if (leaderboard.GetAnimation() === "Idle") {
+        Isaac.DebugString("Fetching Data");
+        fetchLeaderboardData();
+      }
+    } else if (leaderboard.GetAnimation() === "Idle") {
+      Isaac.DebugString("Disappearing");
+      leaderboard.Play("Disappear", true);
+    } else if (
+      !leaderboard.IsFinished("Disappear")
+      && leaderboard.GetAnimation() === "Disappear"
+    ) {
+      Isaac.DebugString("Continuing to update for Disappear");
+      leaderboard.Update();
+      leaderboard.Render(Vector(0, 0));
+    } else if (leaderboard.IsFinished("Disappear")) {
+      Isaac.DebugString("Now Playing Appear");
+      leaderboard.Play("Appear", true);
+    }
+    if (Input.IsActionTriggered(ButtonAction.BOMB, ControllerIndex.KEYBOARD)) {
+      GLOBAL_SELECTED = !GLOBAL_SELECTED;
+      if (GLOBAL_SELECTED) {
+        Isaac.DebugString("Global Selected");
+        leaderboard.SetFrame("Idle", 0);
+      } else {
+        leaderboard.SetFrame("Idle", 2);
       }
     }
   } else {
+    Isaac.DebugString("Back to Appear");
     leaderboard.Play("Appear", true);
-    showLeaderboard = false
+    SHOW_LEADERBOARD = false;
+    GLOBAL_SELECTED = true;
   }
 }
 
-function updateLeaderboard() {
+function fetchLeaderboardData() {
+  Isaac.DebugString("Displaying Data");
+}
 
+function renderHelper() {
+  if (!StatsMenu.IsSecretsMenuVisible()) {
+    const pos = MenuManager.GetViewPosition();
+    helper.Update();
+    helper.Render(Vector(pos.X - 138.5, pos.Y + 1372.5));
+  }
+}
+function updateLeaderboard() {
   const [enabled, requiredSocket] = pcall(require, "socket");
   if (enabled) {
     const socket: Socket | null = requiredSocket as Socket;
-    print(socket)
+    print(socket);
   } else {
     Isaac.DebugString("--luadebug is not enabled.");
   }
@@ -80,14 +113,11 @@ function updateLeaderboard() {
 function findLoginPath() {
   if (
     MenuManager.GetActiveMenu() === MainMenuType.GAME
-    && (Input.IsActionTriggered(ButtonAction.BOMB, ControllerIndex.KEYBOARD)
-      || Input.IsActionTriggered(
-        ButtonAction.BOMB,
-        ControllerIndex.CONTROLLER_1,
-      ))
+    && Input.IsActionTriggered(ButtonAction.BOMB, ControllerIndex.KEYBOARD)
   ) {
     loadInfoBox();
     Isaac.DebugString("test");
+    updateLeaderboard();
   }
 }
 
